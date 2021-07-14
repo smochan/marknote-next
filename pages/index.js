745545a8373ba4c1530f88dbb8ae903edfc4d9cf
smochan <img_2019052@iiitm.ac.in>
2021-07-15 02:38:57 +0530

    a

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,39 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 import queryString from 'query-string'
+import {OAuth2Client} from 'google-auth-library' 
 
 
 
 export default function Home() {
   const router = useRouter()
   const query = router.query
+  const token = ""
+  const CLIENT_ID = "120135436671-sbqei1khck23d092616gduue1s2i2rds.apps.googleusercontent.com"
   useEffect(() => {
     fetch(`https://intense-tundra-92368.herokuapp.com/auth/google/callback?${queryString.stringify(query)}` )
     .then( res => res.json())
-    .then(data => {console.log(data)} )
+    .then(data => {token = data.jwt } )
   })
+
+
+  const client = new OAuth2Client(CLIENT_ID);
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  console.log(payload);
+  // const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+}
+verify().catch(console.error);
+
+
   return (
     <div className={styles.container}>
       <Head>
@@ -23,7 +45,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h2>Marknote</h2>
+        <h1 className={styles.title}>Marknote</h1>
         <p>Login successful</p>
       </main>
       <footer className={styles.footer}>
@@ -31,6 +53,8 @@ export default function Home() {
     </div>
   )
 }
+
+
 
 
 // use router krke.......
