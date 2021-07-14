88d707de0c9d9d67010ebd1d8992be2f478fd54d
smochan <img_2019052@iiitm.ac.in>
2021-07-14 17:46:42 +0530

    abcd

--- pages/api/hello.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default async function handler(req, res) {
-  res.redirect("https://intense-tundra-92368.herokuapp.com/connect/google")
-}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
+
+
 export default function Home() {
+
+  const router = useRouter()
+  const { id_token } = router.query
+  useEffect(() => {
+    fetch(`https://intense-tundra-92368.herokuapp.com/auth/google/callback?id_token=${id_token}` )
+    .then( res => res.json())
+    .then(data => {console.log(data)} )
+  }) 
   return (
     <div className={styles.container}>
       <Head>
@@ -11,12 +23,17 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-      <Link href="https://intense-tundra-92368.herokuapp.com/connect/google">
+      <a href="https://intense-tundra-92368.herokuapp.com/connect/google">
           <button>Login to Google</button>
-        </Link>
+        </a>
       </main>
       <footer className={styles.footer}>
       </footer>
     </div>
   )
 }
+
+
+// use router krke.......
+// use useEffect
+// 
